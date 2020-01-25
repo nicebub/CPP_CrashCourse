f6848db12ae6b75901852d19f5a830bef4ba2c45
Scott Lorberbaum <scott@laureltreecomputing.com>
2020-01-25 13:40:21 -0700

    - updated chapter 5 Bank class to use double for amounts instead 		of long and reflected that in Bank amount methods. 	- copied the Bank Class to chapter 6 and made it into a Bank 	template. Template involves the Logger interface class, 	 Logger inheritance classes, and the Bank class itself to take 	 an argument as to what type the bank account number will be.
    	- The only thing not done yet is to make sure the printf
    		function has the appropriate usage depending on the id type.

--- chptr6/Bank_template.cpp
+++ chptr6/Bank_template.cpp
@@ -4,7 +4,6 @@ struct AccountDatabase {
 	virtual void set_amount(const double amount) = 0;
 	virtual ~AccountDatabase() = default;
 };
-
 struct InMemoryAccountDatabase : AccountDatabase {
 	InMemoryAccountDatabase(double amount) : amount{ amount } {}
 	double get_amount() const override {
@@ -18,31 +17,35 @@ struct InMemoryAccountDatabase : AccountDatabase {
 private:
 	double amount;
 };
+template<typename T>
 struct Logger {
 	virtual ~Logger() = default;
-	virtual void log_transfer(long from, long to, double amount) = 0;
+	virtual void log_transfer(T from, T to, double amount) = 0;
 };
-struct ConsoleLogger : Logger {
+template<typename T>
+struct ConsoleLogger : Logger<T> {
 	ConsoleLogger(const char* tag) : tag{tag} {}
-	void log_transfer(long from, long to, double amount) override {
+	void log_transfer(T from, T to, double amount) override {
 		printf("%s %ld -> %ld: %f\n",tag,from,to,amount);
 	}
 private:
 	const char* tag;
 };
-struct FileLogger : Logger {
-	void log_transfer(long from, long to, double amount) override {
+template<typename T>
+struct FileLogger : Logger<T> {
+	void log_transfer(T from, T to, double amount) override {
 			printf("[file] %ld,%ld,%f\n",from,to,amount);
 		}
 };
+template<typename T>
 struct Bank {
-	Bank(Logger* logger,AccountDatabase& accountdb) : logger{logger},
+	Bank(Logger<T>* logger,AccountDatabase& accountdb) : logger{logger},
 		accountdb{accountdb} {}
 	Bank(AccountDatabase& accountdb) : logger{}, accountdb{accountdb} {}
-	void set_logger(Logger* new_logger) {
+	void set_logger(Logger<T>* new_logger) {
 		logger = new_logger;
 	}
-	void make_transfer(long from, long to, double amount) {
+	void make_transfer(T from, T to, double amount) {
 		if (logger) logger->log_transfer(from,to,amount);
 	}
 	double get_amount() const {
@@ -52,15 +55,15 @@ struct Bank {
 		accountdb.set_amount(id);
 	}
 private:
-	Logger* logger;
+	Logger<T>* logger;
 	AccountDatabase& accountdb;
 };
 
 int main(){
-	ConsoleLogger console_logger("[from console]");
-	FileLogger file_logger;
+	ConsoleLogger<long> console_logger("[from console]");
+	FileLogger<long> file_logger;
 	InMemoryAccountDatabase db1(24);
-	Bank bank(nullptr,db1);
+	Bank<long> bank(nullptr,db1);
 	bank.set_logger(&console_logger);
 	bank.make_transfer(1000,2000,49.95);
 	bank.set_amount(49.95);
@@ -71,5 +74,5 @@ int main(){
 	printf("bank amount: %lf\n",bank.get_amount());
 	long num = 30;
 	bank.set_amount(bank.get_amount()+num);
-	printf("bank amount after adding %ld: %lf\n",num, bank.get_amount());
+	printf("bank amount after adding %ld: %f\n",num, bank.get_amount());
 }
