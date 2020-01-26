26d500d257efaf396d0438a3ca694558a89f57a3
Scott Lorberbaum <scott@laureltreecomputing.com>
2020-01-25 18:35:15 -0700

    I read the description of the problem wrong. 	 Now the AccountDatabase class has a is_in_database method 	 - the InMemoryAccountDatabase class defines the methods 	 - most of the methods now take a long id to look up the id 	 	if its in the database and then can manipulate the amount 		for that particular id. 	- the AccountDatabase class now has an id table and amount table 		to keep track of ID's in the database and their respective 			amounts in the bank. 	- the Bank class now during make transfer will transfer amounts 		from the AccountDatabase from one account to the other if they 		exist. If they don't then an exception is thrown and handled. 		If only have the transfer happens and an exception is thrown, 			then the transfer is reversed so that nothing happens 			as a result besides the error and exception messages.

--- chptr5/Bank.cpp
+++ chptr5/Bank.cpp
@@ -1,37 +1,65 @@
 #include <cstdio>
+#include <stdexcept>
 struct AccountDatabase {
-	virtual double get_amount() const = 0;
-	virtual void set_amount(const double amount) = 0;
+	virtual long is_in_database(const long id) const = 0;
+	virtual double get_amount(const long id) const = 0;
+	virtual void set_amount(const long id,const double amount) = 0;
 	virtual ~AccountDatabase() = default;
 };
 
 struct InMemoryAccountDatabase : AccountDatabase {
-	InMemoryAccountDatabase(double amount) : amount{ amount } {}
-	double get_amount() const override {
-		return amount;
+	InMemoryAccountDatabase(long *ids,double *amounts,const size_t length) : 
+		amount_table{ amounts },
+		id_table{ ids },
+		length {length } {
+			if(!id_table) throw std::runtime_error("id table is NULL");
+			if(!amount_table) throw std::runtime_error("amount table is NULL");
+		}
+
+	long is_in_database(const long id) const override {
+		if(!id_table || !amount_table) return -1;
+		for(auto i{0};i<length;i++)
+			if(id_table[i] == id)
+				return i;
+		return -1;
 	}
-	void set_amount(const double amount) override {
-		this->amount = amount;
+
+	double get_amount(const long id) const override {
+		if(!id_table || !amount_table)
+			throw std::runtime_error("Cannot get amount from uninitialized database");;
+		auto result = this->is_in_database(id);
+		if(result <0) 
+			throw std::logic_error("ID not in database");
+		return amount_table[result];
+		
 	}
-	~InMemoryAccountDatabase(){
+	void set_amount(const long id,const double amount) override {
+		if(!id_table || !amount_table)
+			throw std::runtime_error("Cannot set amount to uninitialized database");
+		auto result = this->is_in_database(id);
+		if(result < 0) 
+			throw std::logic_error("ID not in database");
+		this->amount_table[result] = amount;
 	}
 private:
-	double amount;
+	long *id_table;
+	double *amount_table;
+	size_t length;
 };
 struct Logger {
 	virtual ~Logger() = default;
-	virtual void log_transfer(long from, long to, double amount) = 0;
+	virtual void log_transfer(const long from, const long to, const double amount) const = 0;
 };
 struct ConsoleLogger : Logger {
 	ConsoleLogger(const char* tag) : tag{tag} {}
-	void log_transfer(long from, long to, double amount) override {
+	void log_transfer(const long from,const long to,const double amount) const override {
 		printf("%s %ld -> %ld: %f\n",tag,from,to,amount);
 	}
 private:
 	const char* tag;
 };
 struct FileLogger : Logger {
-	void log_transfer(long from, long to, double amount) override {
+	void log_transfer(const long from, const long to, const double amount) const override {
 			printf("[file] %ld,%ld,%f\n",from,to,amount);
 		}
 };
@@ -42,14 +70,38 @@ struct Bank {
 	void set_logger(Logger* new_logger) {
 		logger = new_logger;
 	}
-	void make_transfer(long from, long to, double amount) {
+	void make_transfer(const long from, const long to, const double amount) {
+		try{
+			this->set_amount(from,this->get_amount(from)-amount);
+		}
+		catch(std::runtime_error &e){
+			printf("%s: %ld\n",e.what(),from);
+			return;
+		}
+		catch(std::logic_error &e){
+			printf("%s: %ld\n",e.what(),from);
+			return;
+		}
+		try{
+			this->set_amount(to,this->get_amount(to)+amount);
+		}
+		catch(std::runtime_error &e){
+			printf("%s: %ld\n",e.what(),to);
+			this->set_amount(from,this->get_amount(from)+amount);
+			return;
+		}
+		catch(std::logic_error &e){
+			printf("%s: %ld\n",e.what(),to);
+			this->set_amount(from,this->get_amount(from)+amount);
+			return;
+		}
 		if (logger) logger->log_transfer(from,to,amount);
 	}
-	double get_amount() const {
-		return accountdb.get_amount();
+	double get_amount(const long id) const {
+		return accountdb.get_amount(id);
 	}
-	void set_amount(const double id) {
-		accountdb.set_amount(id);
+	void set_amount(const long id,const double amount) {
+		accountdb.set_amount(id,amount);
 	}
 private:
 	Logger* logger;
@@ -59,17 +111,42 @@ private:
 int main(){
 	ConsoleLogger console_logger("[from console]");
 	FileLogger file_logger;
-	InMemoryAccountDatabase db1(24);
+	long ids[] = { 1000, 2000, 4000, 3, 45355 };
+	double start_amounts[] { 10000, 40, 30000.67, 24.95, 83553.34 };
+	InMemoryAccountDatabase db1(ids,start_amounts, sizeof(ids)/sizeof(long));
 	Bank bank(nullptr,db1);
 	bank.set_logger(&console_logger);
+
+	printf("before transfer from %ld to %ld:\nfrom: %f to: %f\n",
+	ids[0],ids[1],bank.get_amount(ids[0]),bank.get_amount(ids[1]));
+
 	bank.make_transfer(1000,2000,49.95);
-	bank.set_amount(49.95);
-	printf("bank amount: %lf\n",bank.get_amount());
+
+	printf("after transfer from %ld to %ld:\nfrom: %f to: %f\n",
+	ids[0],ids[1],bank.get_amount(ids[0]),bank.get_amount(ids[1]));
+
+	bank.set_amount(2000,49.95);
+	printf("setting bank amount for account %ld: %lf\n",ids[1],bank.get_amount(2000));
+
 	bank.set_logger(&file_logger);
+
+	printf("before transfer from %ld to %ld:\nfrom: %f to: %f\n",
+	ids[1],ids[2],bank.get_amount(ids[1]),bank.get_amount(ids[2]));
+
 	bank.make_transfer(2000,4000,20.00);
-	bank.set_amount(20.00);
-	printf("bank amount: %lf\n",bank.get_amount());
+
+	printf("after transfer from %ld to %ld:\nfrom: %f to: %f\n",
+	ids[1],ids[2],bank.get_amount(ids[1]),bank.get_amount(ids[2]));
+
+	bank.set_amount(3,20.00);
+	printf("setting bank amount for account %ld: %lf\n",ids[3],bank.get_amount(3));
+
 	long num = 30;
-	bank.set_amount(bank.get_amount()+num);
-	printf("bank amount after adding %ld: %lf\n",num, bank.get_amount());
+	bank.set_amount(ids[4],bank.get_amount(ids[4])+num);
+	printf("setting bank amount for account %ld: %lf\n",ids[4],bank.get_amount(ids[4]));
+
+	printf("bank amount after adding %ld: %lf\n",num, bank.get_amount(ids[4]));
+	bank.make_transfer(6000,3000,78.34);
+	bank.make_transfer(2000,3000,78.34);
+	bank.make_transfer(6000,3,78.34);
 }
--- chptr6/Bank_template.cpp
+++ chptr6/Bank_template.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <stdexcept>
 struct AccountDatabase {
 	virtual double get_amount() const = 0;
 	virtual void set_amount(const double amount) = 0;
@@ -12,11 +13,40 @@ struct InMemoryAccountDatabase : AccountDatabase {
 	void set_amount(const double amount) override {
 		this->amount = amount;
 	}
-	~InMemoryAccountDatabase(){
-	}
 private:
 	double amount;
 };
+
+struct Account {
+	Account(long id,AccountDatabase & accountdb)
+		 : id{ id },
+			accountdb{ accountdb }{
+	}
+	long get_id() const {
+		return id;
+	}
+	void set_id(const long id){
+		this->id = id;
+	}
+	double get_balance() const {
+		return accountdb.get_amount();
+	}
+	void set_balance(const double balance) {
+		accountdb.set_amount(balance);
+	}
+	void add_to_balance(const double amount){
+		accountdb.set_amount(accountdb.get_amount() + amount);
+	}
+	void remove_from_balance(const double amount){
+		if(amount > accountdb.get_amount())
+			throw std::runtime_error("Not enough money in account");
+		accountdb.set_amount(accountdb.get_amount() - amount);
+	}
+private:
+	long id;
+	AccountDatabase& accountdb;
+};
+
 template<typename T>
 struct Logger {
 	virtual ~Logger() = default;
@@ -75,4 +105,14 @@ int main(){
 	long num = 30;
 	bank.set_amount(bank.get_amount()+num);
 	printf("bank amount after adding %ld: %f\n",num, bank.get_amount());
+	
+	ConsoleLogger<Account> account_logger("[account]");
+
+	Bank<Account> bank2(&account_logger,db1);
+	
+	bank2.set_logger(&account_logger);
+	Account checking{1000,db1};
+	Account savings{2000,db1};
+	bank2.make_transfer(checking,savings,49.95);
+	
 }
